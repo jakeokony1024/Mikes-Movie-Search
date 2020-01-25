fc7fe149cb768df99be667428b8a354e3f7a0fcc
Jakeokony1024 <jakeokony@gmail.com>
2020-01-25 11:33:48 -0600

    added stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,33 +10,10 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig)
 var database = firebase.database()
+var searchResults = "";
 
-var utellySettings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=uk",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-		"x-rapidapi-key": "31be13b542msh7b90b2af5235426p12bd4bjsnac8785b93464"
-	}
-}
 
-$.ajax(utellySettings).done(function (response) {
-	console.log(response);
-});
 
-var IMDBsettings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/inception",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
-		"x-rapidapi-key": "31be13b542msh7b90b2af5235426p12bd4bjsnac8785b93464"
-	}
-}
-
-$.ajax(IMDBsettings).done(function (response) {
-	console.log(response);
-});
+$(document).ready(function(){
+	$("#searchResults").
+})
--- assets/javascript/config.js
+++ assets/javascript/config.js
@@ -9,10 +9,12 @@ $("#find-movie").on("click", function (event) {
     // event.preventDefault() can be used to prevent an event's default behavior.
     // Here, it prevents the submit button from trying to submit a form when clicked
     event.preventDefault();
+
     // Here we grab the text from the input box
-    var movie = $("#movie-input").val();
-    $("#movie-input").empty(); //clears out the div
-    //=================================================================================================================================
+    var movie = $("#movie-input").val().trim();
+ 
+    // $("#movie-input").empty(); //clears out the div
+    
     const url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=' + movie + '&country=us'
     const options = {
         method: 'GET',
@@ -25,29 +27,55 @@ $("#find-movie").on("click", function (event) {
         .then((response) => {
             return response.json();
         })
+
         .then((myJson) => {
-            //  console.log(myJson);
+            
             var utellyResp = (myJson);
             console.log(utellyResp);
-            //$("#movie-view").text(JSON.stringify(myJson));
+            
 
             console.log(utellyResp.results);
             console.log(utellyResp.results[0].name);
             console.log(utellyResp.results[0].locations[0].display_name);
 
             //Loop through to get movie name
-            for (i = 0; i < utellyResp.results.length +5; i++) {
+            for (i = 0; i < utellyResp.results.length; i++) {
                 console.log(utellyResp.results[i].name);
+                
+                $("#movie-view").append("<a href ='https://mnezz1131.github.io/mikes-movie/movie.html'> <img src= " + utellyResp.results[i].picture + "></a>");
                 $("#movie-view").append("<ul>" + utellyResp.results[i].name + "</ul>");
                 $("#movie-view").append("<ul>" + utellyResp.results[i].locations[i].display_name + "</ul>");
-                $("#movie-view").append("<a href ='https://mnezz1131.github.io/mikes-movie/movie.html'> <img src= " + utellyResp.results[i].picture + "></a>");
-            }
+            } 
+            
             //Loop to get streaming service
             for (a = 0; a < utellyResp.results[0].locations.length; a++) {
                 console.log(utellyResp.results[0].locations[a].display_name);
             }
         });
-
-       
 });
+
+var config = {
+    apiKey: "AIzaSyCGYhdtL7ScaEYcZwM31u3L-A5GWEdgkG4",
+    authDomain: "mikes-movies.firebaseapp.com",
+    databaseURL: "https://mikes-movies.firebaseio.com",
+    projectId: "mikes-movies",
+    storageBucket: "mikes-movies.appspot.com"
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
+var searchResults = {
+    response: utellyResp,
+    name: name,
+    display_name: display_name,
+    locations: locations,
+    picture: picture
+}
+$("#find-movie").on("click", function() {
+    database.ref().set({
+      searchResults: searchResults
+    });
+  });
+
+
 //================================================================================================================================     
