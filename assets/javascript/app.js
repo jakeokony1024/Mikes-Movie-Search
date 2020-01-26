9e52bc3f501da9de1b6ac658fcb2ef660074a825
Christian Kimura <christiankimura94@gmail.com>
2020-01-26 15:23:57 -0600

    adding much more firebase functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,53 @@
-
-//firebase calling
-var firebaseConfig = {
+var config = {
     apiKey: "AIzaSyCGYhdtL7ScaEYcZwM31u3L-A5GWEdgkG4",
     authDomain: "mikes-movies.firebaseapp.com",
     databaseURL: "https://mikes-movies.firebaseio.com",
     projectId: "mikes-movies",
-    storageBucket: "mikes-movies.appspot.com"
+    storageBucket: "mikes-movies.appspot.com",
 };
+firebase.initializeApp(config);
+var database = firebase.database();
 
-firebase.initializeApp(firebaseConfig)
-var database = firebase.database()
+var utellyResp;
+var display_name;
+var locations;
+var name;
+var picture;
+var display_name;
+var movie;
 
-var utellySettings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=uk",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-		"x-rapidapi-key": "31be13b542msh7b90b2af5235426p12bd4bjsnac8785b93464"
-	}
-}
+$(document).ready(function () {
+	console.log("ready!")
+	//Loop to get streaming service
 
-$.ajax(utellySettings).done(function (response) {
-	console.log(response);
-});
+	database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
+		console.log(snapshot.val());
+		movie = snapshot.val();
+	 })
 
-var IMDBsettings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/inception",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
-		"x-rapidapi-key": "31be13b542msh7b90b2af5235426p12bd4bjsnac8785b93464"
-	}
-}
 
-$.ajax(IMDBsettings).done(function (response) {
-	console.log(response);
+
+
+
+	
+	const url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=' + movie + '&country=us'
+    const options = {
+        method: 'GET',
+        headers: {
+            "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+            "x-rapidapi-key": "dfb87b0ec6msh548e75d6f762a4bp1dc2f1jsn322c78d86502"
+        },
+    }
+    fetch(url, options)
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+			utellyResp = (myJson);
+		});
+
+		for (a = 0; a < utellyResp.results[0].locations.length; a++) {
+			console.log(utellyResp.results[0].locations[a].display_name);
+		}
+		
 });
