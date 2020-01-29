69aaed9ba55a0e3b83a72da35a3fb06ee8ffd602
Christian Kimura <christiankimura94@gmail.com>
2020-01-28 21:54:03 -0600

    adding the app and config we worked on during class

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,22 +14,23 @@ var locations;
 var name;
 var picture;
 var display_name;
-var movie;
+var movie = "";
+var pushArray = [];
 
 $(document).ready(function () {
 	console.log("ready!")
 	//Loop to get streaming service
 
 	database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
-		console.log(snapshot.val());
-		movie = snapshot.val();
-	 })
+        console.log(snapshot.val());
+        var addedObject = {};
+     })
 
+    $(document).on("click", ".movSelect", function(e){
+        e.preventDefault();
+        console.log(e.target.value);
+    })
 
-
-
-
-	
 	const url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=' + movie + '&country=us'
     const options = {
         method: 'GET',
@@ -45,9 +46,13 @@ $(document).ready(function () {
         .then((myJson) => {
 			utellyResp = (myJson);
 		});
-
+        // console.log(utellyResp)
+        // console.log(utellyResp.results);
+        // console.log(utellyResp.results[0].name);
+        // console.log(utellyResp.results[0].locations[0].display_name);
 		for (a = 0; a < utellyResp.results[0].locations.length; a++) {
+            provider = (utellyResp.results[0].locations[a].display_name);
 			console.log(utellyResp.results[0].locations[a].display_name);
 		}
-		
-});
+	
+    });
