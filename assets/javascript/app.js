9df9d489dda77a94d35a6822808eae4246dc3257
Jakeokony1024 <jakeokony@gmail.com>
2020-01-30 15:36:27 -0600

    updated code to match, added sticky footer,onclick to config

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,13 @@
-var config = {
-    apiKey: "AIzaSyCGYhdtL7ScaEYcZwM31u3L-A5GWEdgkG4",
-    authDomain: "mikes-movies.firebaseapp.com",
-    databaseURL: "https://mikes-movies.firebaseio.com",
-    projectId: "mikes-movies",
-    storageBucket: "mikes-movies.appspot.com",
-};
-firebase.initializeApp(config);
-var database = firebase.database();
-
-var utellyResp;
-var display_name;
-var locations;
-var name;
-var picture;
-var display_name;
-var movie;
-var onClickItem;
-
 $(document).ready(function () {
-	console.log("ready!")
-	//Loop to get streaming service
-
-	database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
-		console.log(snapshot.val());
-        movie = snapshot.val();
-        console.log(onClickItem);
-	 })
-
-	const url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=' + movie + '&country=us'
+    console.log("ready!")
+    
+    //Set up gobal variables
+    var utellyResp;
+    //Getting the name from local storage
+    console.log(localStorage.getItem("storageName"));
+    let moviename = (localStorage.getItem("storageName"))
+    //Utelly API call to get the show that was searched for to see where it's streaming    
+    const url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=' + moviename + '&country=us'
     const options = {
         method: 'GET',
         headers: {
@@ -40,43 +20,90 @@ $(document).ready(function () {
             return response.json();
         })
         .then((myJson) => {
-			utellyResp = (myJson);
-        });
-    
-        var APIKey ="AIzaSyBBhRn34PTtR-EyygLxeptxYiPc9ThiQr8"
-        // -----------------------------------------------------------------------
-        function getVideo() {
-          $.ajax({
-            type: 'GET',
-            url: 'https://www.googleapis.com/youtube/v3/search',
-            data: {
-                key: 'AIzaSyBBhRn34PTtR-EyygLxeptxYiPc9ThiQr8',
-                q: "wonder woman 1984 trailer",
-                part: 'snippet',
-                maxResults: 1,
-                type: 'video',
-                videoEmbeddable: true,
-            },
-            success: function(data){
-                embedVideo(data)
-                console.log(data);
-            },
-            error: function(response){
-                console.log("Request Failed");
-                console.log(response);
-            }
-          });
-        }
-        function embedVideo(data) {
-        $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
-        $('h3').text(data.items[0].snippet.title)
-        $('.description').text(data.items[0].snippet.description)
-    }
-    getVideo();
-    
+            utellyResp = (myJson);
+                       
+            
+            (utellyResp.results[0].locations[0].url);
+            
+            var movieName = utellyResp.results[0].name;
+            
+            var streamer = utellyResp.results[0].locations[0].url
 
-		// for (a = 0; a < utellyResp.results[0].locations.length; a++) {
-		// 	console.log(utellyResp.results[0].locations[a].display_name);
-		// }
-		
+            $("#stream-view").append(streamer)
+
+            for (i = 0; i < utellyResp.results.length; i++) {
+                for (i = 0; i < utellyResp.results.length; i++) {
+                    
+                    var movieDiv = $("<div>"); //Jquery to make a Movie Div
+
+                    movieDiv.addClass("movieDiv"); //Adding Bootstrap Class to position images
+
+                    var name = utellyResp.results[0].name; //Loop through UTELLY Json to get movie name 
+
+                    var p = $("<p>").text(movieName); //Setup a <p> tage for name
+
+                    movieDiv.prepend(p); //Adds <p> before the movie image to the div   
+
+                    $("#movie-view").text(name); // Appends the DIV to the movie
+                }
+
+                //YOU TUBE API TRAILERS IS WORKING!!
+                //=========================================================================================================================================
+                var APIKey = "AIzaSyBBhRn34PTtR-EyygLxeptxYiPc9ThiQr8"
+                // -----------------------------------------------------------------------
+                function getVideo() {
+                    $.ajax({
+                        type: 'GET',
+                        url: 'https://www.googleapis.com/youtube/v3/search',
+                        data: {
+                            key: APIKey,
+                            q: movieName + 'trailer',
+                            part: 'snippet',
+                            maxResults: 1,
+                            type: 'video',
+                            videoEmbeddable: true,
+                            fs: 1
+                        },
+                        success: function (data) {
+                            embedVideo(data)
+                            console.log(data);
+                        },
+                        error: function (response) {
+                            console.log("Request Failed");
+                            console.log(response);
+                        }
+                    });
+                }
+
+
+                function embedVideo(data) {
+                    $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
+                    $('h3').text(data.items[0].snippet.title)
+                    $('.description').text(data.items[0].snippet.description)
+                }
+
+                getVideo();
+
+            };
+
+        });
+
+    function getImdb(){
+        var settings = {
+            async: true,
+            crossDomain: true,
+            queryURL: "https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=",
+            method: "GET",
+            headers: {
+                "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+                "x-rapidapi-key": "a319d638b0msh397c0e24b21a62fp1a2660jsnc7f7e0f81537"
+            }
+        }
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+              console.log(response)
+          });    
+    };
 });
