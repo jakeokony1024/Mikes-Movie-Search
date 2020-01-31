684385a7ff93d6e43a98190dd59a2d20f9a2b808
Christian Kimura <christiankimura94@gmail.com>
2020-01-30 21:03:51 -0600

    updating js files with better logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,20 @@
-var config = {
-    apiKey: "AIzaSyCGYhdtL7ScaEYcZwM31u3L-A5GWEdgkG4",
-    authDomain: "mikes-movies.firebaseapp.com",
-    databaseURL: "https://mikes-movies.firebaseio.com",
-    projectId: "mikes-movies",
-    storageBucket: "mikes-movies.appspot.com",
-};
-firebase.initializeApp(config);
-var database = firebase.database();
-
-var utellyResp;
-var display_name;
-var locations;
-var name;
-var picture;
-var display_name;
-var movie = "";
-var pushArray = [];
-
 $(document).ready(function () {
-	console.log("ready!")
-	//Loop to get streaming service
-
-	database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
-        console.log(snapshot.val());
-        var addedObject = {};
-     })
-
-    $(document).on("click", ".movSelect", function(e){
-        e.preventDefault();
-        console.log(e.target.value);
-    })
-
-	const url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=' + movie + '&country=us'
+    console.log("ready!")
+    //Set up gobal variables
+    var utellyResp;
+    var name;
+    //Getting the name from local storage
+    // console.log(localStorage.getItem("movieName"));
+    let moviename = (localStorage.getItem("storageName"))
+    //Create structure for displaying movie name======================================================
+    var titleDiv = $("<div>"); //Jquery to make a Movie Div
+    titleDiv.addClass("titleDiv"); //Adding Bootstrap Class to position images
+    var p = $("<p>").text(moviename); //Setup a <p> tage for name
+    p.addClass("localName");
+    $("#title-view").prepend(p); // Appends the DIv to the movie
+    //================================================================================================================================     
+    //Utelly API call to get the show that was searched for to see where it's streaming    
+    const url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=' + moviename + '&country=us'
     const options = {
         method: 'GET',
         headers: {
@@ -44,15 +27,50 @@ $(document).ready(function () {
             return response.json();
         })
         .then((myJson) => {
-			utellyResp = (myJson);
-		});
-        // console.log(utellyResp)
-        // console.log(utellyResp.results);
-        // console.log(utellyResp.results[0].name);
-        // console.log(utellyResp.results[0].locations[0].display_name);
-		for (a = 0; a < utellyResp.results[0].locations.length; a++) {
-            provider = (utellyResp.results[0].locations[a].display_name);
-			console.log(utellyResp.results[0].locations[a].display_name);
-		}
-	
-    });
+            utellyResp = (myJson);
+            //Testing -Console logs to deteremine where the data fields we want are            
+            //Loop through to get movie name
+            for (a = 0; a < utellyResp.results[0].locations.length; a++) {
+                console.log(utellyResp.results[0].locations[a].display_name);
+                var streamDiv = $("<div>"); //Jquery to make a Movie Div
+                streamDiv.addClass("streamDiv"); //Adding Bootstrap Class to position images
+                var stream = utellyResp.results[0].locations[a].display_name; //Loop through UTELLY Json to get movie name                   
+                var p = $("<p>").text(stream); //Setup a <p> tage for name
+                $("#stream-view").append(p); // Appends the DIv to the movie-view section of HTML 
+            }
+        });
+    //YOU TUBE API TRAILERS IS WORKING!!
+    //=========================================================================================================================================
+    var APIKey = "AIzaSyBBhRn34PTtR-EyygLxeptxYiPc9ThiQr8"
+    // -----------------------------------------------------------------------
+    function getVideo() {
+        $.ajax({
+            type: 'GET',
+            url: 'https://www.googleapis.com/youtube/v3/search',
+            data: {
+                key: 'AIzaSyBBhRn34PTtR-EyygLxeptxYiPc9ThiQr8',
+                q: moviename + 'trailer',
+                //q:  movieName +'trailer',
+                part: 'snippet',
+                maxResults: 1,
+                type: 'video',
+                videoEmbeddable: true,
+            },
+            success: function (data) {
+                embedVideo(data)
+                console.log(data);
+            },
+            error: function (response) {
+                console.log("Request Failed");
+                console.log(response);
+            }
+        });
+    }
+    //You Tube function to embed video trailer=================================================================
+    function embedVideo(data) {
+        $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
+        $('h3').text(data.items[0].snippet.title)
+        $('.description').text(data.items[0].snippet.description)
+    }
+    getVideo();
+});
--- assets/javascript/config.js
+++ assets/javascript/config.js
@@ -1,7 +1,5 @@
 //Jquery Document.Ready function
 $(document).ready(function () {
-    console.log("ready!");
-
     //Set up gobal variables
     var utellyResp;
     var display_name;
@@ -9,21 +7,8 @@ $(document).ready(function () {
     var name;
     var picture;
     var provider;
-    var movName;
-    var pushArray = [];
-
-    // Configure the Fireabase Database
-    var config = {
-        apiKey: "AIzaSyCGYhdtL7ScaEYcZwM31u3L-A5GWEdgkG4",
-        authDomain: "mikes-movies.firebaseapp.com",
-        databaseURL: "https://mikes-movies.firebaseio.com",
-        projectId: "mikes-movies",
-        storageBucket: "mikes-movies.appspot.com",
-    };
-
-    //Initialize the firebase database
-    firebase.initializeApp(config);
-    var database = firebase.database();
+    var movLink = [];
+    var movieName;
 
     //Sets up onclick funtion to capture the show that was search for
     $('#find-movie').on("click", function (event) {
@@ -47,57 +32,54 @@ $(document).ready(function () {
             })
             .then((myJson) => {
                 utellyResp = (myJson);
-
                 //Testing -Console logs to deteremine where the data fields we want are            
-                    console.log(utellyResp);
-                    console.log(utellyResp.results);
-                    console.log(utellyResp.results[0].name);
-                    console.log(utellyResp.results[0].locations[0].display_name);
-
+                //    console.log(utellyResp);
+                //   console.log(utellyResp.results);
+                //    console.log(utellyResp.results[0].name);
+                //    console.log(utellyResp.results[0].locations[0].display_name);
+                //    console.log(utellyResp.results[0].locations[0].url);
                 //Loop through to get movie name
-                for (i = 0; i < utellyResp.results.length; i++) {
-                    //console.log(utellyResp.results[i].name);
-                    
-                    var movieDiv = $("<div>"); //Jquery to make a Movie Div
-                    movieDiv.addClass("movieDiv float-left"); //Adding Bootstrap Class to position images
-                    var name = utellyResp.results[i].name; //Loop through UTELLY Json to get movie name 
-                    var p = $("<p class=movSelect>").text(name); //Setup a <p> tage for name
-                    var movieImage = $("<img class=movSelect>"); //creates an <img> tag on HTML 
-                    movieImage.attr("src", utellyResp.results[i].picture); //Set img src attribute
-                    link = $("<a>");
-                    link.attr("href", "movie.html");
-                    link.addClass("link");
-                    p.attr("value", name);
-                    p.addClass("p-movieName")
-                    link.addClass("rounded"); //Adds Bootstrap class to round edges of image   
-                    link.attr("target", "_blank")
-                    movieDiv.prepend(p); //Adds <p> before the movie image to the div   
-                    movieDiv.prepend(movieImage); //Adds the movieimage to the div   
-                    link.append(movieDiv);
-                    $("#movie-view").append(link); // Appends the DIv to the movie-view section of HTML   
-                    
+                // for (i = 0; i < utellyResp.results.length; i++) {
+                //     var movieDiv = $("<div>"); //Jquery to make a Movie Div
+                //     movieDiv.addClass("movieDiv float-left"); //Adding Bootstrap Class to position images
+                //     var name = utellyResp.results[i].name; //Loop through UTELLY Json to get movie name 
+                //     var p = $("<p>").text(name); //Setup a <p> tage for name
+                //     movieDiv.attr("data-name", name);//gives each <p> tag matching data name with name
+                //     var movieImage = $("<img>"); //creates an <img> tag on HTML
+                //     movieImage.attr("src", utellyResp.results[i].picture); //Set img src attribute
+                //     link = $("<a>");
+                //     link.data("href", "movie.html");
+                //     link.data("movieName", name);
+                //     link.addClass("link");
+                //     link.addClass("movieAtag")
+                //     link.addClass("rounded"); //Adds Bootstrap class to round edges of image   
+                //     link.attr("target", "_blank")
+                //     movieDiv.prepend(p); //Adds <p> before the movie image to the div   
+                //     movieDiv.prepend(movieImage); //Adds the movieimage to the div   
+                //     link.append(movieDiv);
+                //     $("#movie-view").append(link); // Appends the DIv to the movie-view section of HTML   
+                // }
 
-                    var tempObject = {
-                        searchResults: movie,
-                        moviename: name,
-                        dateAdded: Date.now()
-                    }
-                    database.ref().push(tempObject);
-                    console.log(tempObject)
 
-                    //Original Code for looping and finding the search results
-                    //$("#movie-view").append("<ul>"+utellyResp.results[i].name+"</ul>");
-                    //$("#movie-view").append("<a href ='https://mnezz1131.github.io/mikes-movie/movie.html'> <img src= "+utellyResp.results[i].picture+"></a>");
-                }
-                //Loop to get streaming service
-                for (a = 0; a < utellyResp.results[0].locations.length; a++) {
-                    // console.log(utellyResp.results[0].locations[a].display_name);
-                    provider = (utellyResp.results[0].locations[a].display_name);
-                    console.log(provider);
-                }
+                //updated and more efficient than previous 'for' loop
+                var movies = utellyResp.results
+                movies.map(movie => {
+                    var movieHtml =
+                        `<a href='movie.html' data-moviename='${movie.name}' class='link movieAtag rounded' target='_blank'>
+                                 <div class='movieDiv float-left'>
+                                         <img src='${movie.picture}' />
+                                         <p data-name='${movie.name}'>${movie.name}</p>
+                                 </div>
+                         </a>`;
+                    $("#movie-view").append(movieHtml);
+                })
+                $(document).on("click", ".movieAtag", function () {
+                    movieName = localStorage.setItem("storageName", $(this).data("moviename"))
+                });
+                localStorage.setItem("storageName", movieName);
             });
-            
     });
+
     //================================================================================================================================    
     //End tag for Document.Ready
 });
