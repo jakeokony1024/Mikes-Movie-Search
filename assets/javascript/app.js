28faf46535355f9c9d1fedb6444c750241b7262d
Mike Nezzer <mnezz1131@gmail.com>
2020-01-31 18:18:58 -0600

    close to finish line

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,22 @@
-<<<<<<< HEAD
 $(document).ready(function () {
     console.log("ready!")
     //Set up gobal variables
     var utellyResp;
     var name;
+    var streamUrl;
     //Getting the name from local storage
-  
-  // console.log(localStorage.getItem("movieName"));
-    let moviename = (localStorage.getItem("storageName"))  
-    
-//Create structure for displaying movie name======================================================
+
+    // console.log(localStorage.getItem("movieName"));
+    let moviename = (localStorage.getItem("storageName"))
+
+    //Create structure for displaying movie name======================================================
     var titleDiv = $("<div>"); //Jquery to make a Movie Div
     titleDiv.addClass("titleDiv"); //Adding Bootstrap Class to position images
     var p = $("<p>").text(moviename); //Setup a <p> tage for name
-    p.addClass("localName");   
+    p.addClass("title");
     $("#title-view").prepend(p); // Appends the DIv to the movie
-
-//================================================================================================================================     
-//Utelly API call to get the show that was searched for to see where it's streaming    
-const url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=' + moviename + '&country=us'
-const options = {
-    method: 'GET',
-    headers: {
-        "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-        "x-rapidapi-key": "dfb87b0ec6msh548e75d6f762a4bp1dc2f1jsn322c78d86502"
-    },
-}
-fetch(url, options)
-    .then((response) => {
-        return response.json();
-    })
-    .then((myJson) => {
-        utellyResp = (myJson);
-        //Testing -Console logs to deteremine where the data fields we want are            
-       
-        //Loop through to get movie name
-        for (a = 0; a < utellyResp.results[0].locations.length; a++) {
-            console.log(utellyResp.results[0].locations[a].display_name);
-            console.log(utellyResp.results[0].locations[a].url);
-            var streamDiv = $("<div>"); //Jquery to make a Movie Div
-            streamDiv.addClass("streamDiv"); //Adding Bootstrap Class to position images
-            var stream = utellyResp.results[0].locations[a].display_name; //Loop through UTELLY Json to get movie name                   
-       
-            var p = $("<p>").text(stream); //Setup a <p> tage for name
-      
-            $("#stream-view").append(p); // Appends the DIv to the movie-view section of HTML 
-
-                   
-        }
-
-        
-    });
-=======
-
-$(document).ready(function () {
-    console.log("ready!")
-    getImdb();
-    
-    //Set up gobal variables
-    var utellyResp;
-    //Getting the name from local storage
-    console.log(localStorage.getItem("storageName"));
-    let moviename = (localStorage.getItem("storageName"))
+    $(".result").text("  " + moviename);
+    //================================================================================================================================     
     //Utelly API call to get the show that was searched for to see where it's streaming    
     const url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=' + moviename + '&country=us'
     const options = {
@@ -78,132 +33,109 @@ $(document).ready(function () {
         .then((myJson) => {
             utellyResp = (myJson);
             //Testing -Console logs to deteremine where the data fields we want are            
-            console.log(utellyResp);
-            console.log(utellyResp.results);
-            console.log(utellyResp.results[0].name);
-            console.log(utellyResp.results[0].locations[0].display_name);
-            (utellyResp.results[0].locations[0].url);
-            console.log("This is the Movie Name - " + utellyResp.results[0].name);
-            var movieName = utellyResp.results[0].name;
-            console.log(utellyResp.results[0].locations[0].display_name);
-            console.log("This is the Streamer URL- " + utellyResp.results[0].locations[0].url);
-            var streamer = utellyResp.results[0].locations[0].url
-            
-            $("#stream-view").append(streamer)
 
-            for (i = 0; i < utellyResp.results.length; i++) {
-                for (i = 0; i < utellyResp.results.length; i++) {
-                    
-                    var movieDiv = $("<div>"); //Jquery to make a Movie Div
-                    movieDiv.addClass("movieDiv"); //Adding Bootstrap Class to position images
-                    var name = utellyResp.results[0].name; //Loop through UTELLY Json to get movie name 
-                    var p = $("<p>").text(movieName); //Setup a <p> tage for name
+            //Loop through to get movie name
+            for (a = 0; a < utellyResp.results[0].locations.length; a++) {
+                console.log(utellyResp.results[0].locations[a].display_name);
+                streamUrl = utellyResp.results[0].locations[a].url
+                console.log(streamUrl);
+                var streamDiv = $("<div>"); //Jquery to make a Movie Div
+                streamDiv.addClass("streamDiv"); //Adding Bootstrap Class to position images
+                var stream = utellyResp.results[0].locations[a].display_name; //Loop through UTELLY Json to get movie name                   
 
-                    movieDiv.prepend(p); //Adds <p> before the movie image to the div   
-
-                    $("#movie-view").text(name); // Appends the DIV to the movie
-                }
->>>>>>> 9885e4fb350630b8a79defc1c8e87c6ae218e8a8
-
-                //YOU TUBE API TRAILERS IS WORKING!!
-                //=========================================================================================================================================
-                var APIKey = "AIzaSyBBhRn34PTtR-EyygLxeptxYiPc9ThiQr8"
-                // -----------------------------------------------------------------------
-                function getVideo() {
-                    $.ajax({
-                        type: 'GET',
-                        url: 'https://www.googleapis.com/youtube/v3/search',
-                        data: {
-                            key: APIKey,
-                            q: movieName + 'trailer',
-                            part: 'snippet',
-                            maxResults: 1,
-                            type: 'video',
-                            videoEmbeddable: true,
-                            fs: 1
-                        },
-                        success: function (data) {
-                            embedVideo(data)
-                            console.log(data);
-                        },
-                        error: function (response) {
-                            console.log("Request Failed");
-                            console.log(response);
-                        }
-                    });
-                }
+                var p = $("<p>").text(stream); //Setup a <p> tage for name
+                p.addClass("str")
+                
+                $("#stream-view").append(p); // Appends the DIv to the movie-view section of HTML 
 
 
-<<<<<<< HEAD
+            }
 
 
+        });
+    //   http://www.omdbapi.com/?i=tt3896198&apikey=bbe0873c
+    //===========================================================================================================
+    var queryURL = "https://www.omdbapi.com/?t=" + moviename + "&apikey=bbe0873c";
+
+    // Creating an AJAX call for IMDB
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        //Creating a Div for the plot
+        var movieDiv = $("<div class='movie'>");
+        // Storing the plot
+        var plot = response.Plot;
+        // Creating an element to hold the plot
+        var pThree = $("<p>").text("Plot: " + plot);
+        // Appending the plot
+        movieDiv.append(pThree);
+        //Creating a Div for the Rating, Image and Release
+        var mviewDiv = $("<div class='m-view'>");
+        // Storing the rating data
+        var rating = response.Rated;
+        // Creating an element to have the rating displayed
+        var pOne = $("<p>").text("Rating: " + rating);
+        pOne.addClass("rating")
+        // Displaying the rating
+        mviewDiv.append(pOne);
+        // Storing the release year
+        var released = response.Released;
+        // Creating an element to hold the release year 
+        var pTwo = $("<p>").text("Released: " + released);
+        pTwo.addClass("release")
+        //Displaying the release year
+        mviewDiv.append(pTwo);
+        // Retrieving the URL for the image
+        var imgURL = response.Poster;
+        // Creating an element to hold the image
+        var image = $("<img>").attr("src", imgURL);
+        image.addClass("img")
+        // Appending the image
+        mviewDiv.append(image);
+        // Putting the information in the DIVS
+        $("#movies-view").prepend(movieDiv);
+
+        $("#m-view").prepend(mviewDiv);
+    });
 
     //YOU TUBE API TRAILERS IS WORKING!!
     //=========================================================================================================================================
-    var APIKey = "AIzaSyBBhRn34PTtR-EyygLxeptxYiPc9ThiQr8"
+    var APIKey ="AIzaSyBBhRn34PTtR-EyygLxeptxYiPc9ThiQr8"
     // -----------------------------------------------------------------------
     function getVideo() {
-        $.ajax({
-            type: 'GET',
-            url: 'https://www.googleapis.com/youtube/v3/search',
-            data: {
-                key: 'AIzaSyBBhRn34PTtR-EyygLxeptxYiPc9ThiQr8',
-                q: moviename + 'trailer',
-                //q:  movieName +'trailer',
-                part: 'snippet',
-                maxResults: 1,
-                type: 'video',
-                videoEmbeddable: true,
-            },
-            success: function (data) {
-                embedVideo(data)
-                console.log(data);
-            },
-            error: function (response) {
-                console.log("Request Failed");
-                console.log(response);
-            }
-        });
-    }
-    //You Tube function to embed video trailer=================================================================
-    function embedVideo(data) {
-        $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
-        $('h3').text(data.items[0].snippet.title)
-        $('.description').text(data.items[0].snippet.description)
-    }
-
-    getVideo();
-});
-=======
-                function embedVideo(data) {
-                    $('iframe').attr(' src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
-                    $('h3').text(data.items[0].snippet.title)
-                    $('.description').text(data.items[0].snippet.description)
-                }
-
-                getVideo();
-
-            };
-
-        });
-
-    function getImdb(){
-        var settings = {
-            async: true,
-            crossDomain: true,
-            queryURL: "https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=",
-            method: "GET",
-            headers: {
-                "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
-                "x-rapidapi-key": "a319d638b0msh397c0e24b21a62fp1a2660jsnc7f7e0f81537"
-            }
+      $.ajax({
+        type: 'GET',
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        data: {
+            key: 'AIzaSyBBhRn34PTtR-EyygLxeptxYiPc9ThiQr8',
+            q: moviename + "trailer",
+            part: 'snippet',
+            maxResults: 1,
+            type: 'video',
+            videoEmbeddable: true,
+        },
+        success: function(data){
+            embedVideo(data)
+            console.log(data);
+        },
+        error: function(response){
+            console.log("Request Failed");
+            console.log(response);
         }
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-          }).then(function(response) {
-              console.log(response)
-          });    
-    };
+      });
+    }
+    
+
+    function embedVideo(data) {
+    $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
+    $('h3').text(data.items[0].snippet.title)
+    $('.description').text(data.items[0].snippet.description)
+}
+
+
+getVideo();
+
+
 });
->>>>>>> 9885e4fb350630b8a79defc1c8e87c6ae218e8a8
--- assets/javascript/config.js
+++ assets/javascript/config.js
@@ -1,11 +1,6 @@
 //Jquery Document.Ready function
 $(document).ready(function () {
-<<<<<<< HEAD
     console.log("ready!");
-=======
-
-    
->>>>>>> 9885e4fb350630b8a79defc1c8e87c6ae218e8a8
     //Set up gobal variables
 
     var utellyResp;
@@ -13,32 +8,20 @@ $(document).ready(function () {
     var locations;
     var name;
     var picture;
-<<<<<<< HEAD
     var movieName;
     var movieAtag;
     var movieDiv;
-=======
-    var provider;
-    var movLink = "";
->>>>>>> 9885e4fb350630b8a79defc1c8e87c6ae218e8a8
-
+    var p;
     //Sets up onclick funtion to capture the show that was search for
 
     $('#find-movie').on("click", function (event) {
         event.preventDefault();
         //Geting the value of text entered in  the input box 
         var movie = $("#movie-input").val().trim();
-<<<<<<< HEAD
         console.log("This is the search result: " + movie);
         //Get Input and send to local storage
         //var getInput = movie
         //localStorage.setItem("storageName",getInput);
-=======
-        console.log("This is the movie: " + movie);
-        // Get Input and send to local storage
-        var getInput = movie
-        localStorage.setItem("storageName",getInput);
->>>>>>> 9885e4fb350630b8a79defc1c8e87c6ae218e8a8
 
         //Utelly API call to get the show that was searched for to see where it's streaming    
         const url = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=' + movie + '&country=us'
@@ -56,7 +39,6 @@ $(document).ready(function () {
             .then((myJson) => {
                 utellyResp = (myJson);
                 //Testing -Console logs to deteremine where the data fields we want are            
-<<<<<<< HEAD
                 console.log(utellyResp);
                 console.log(utellyResp.results);
                 console.log(utellyResp.results[0].name);
@@ -72,7 +54,7 @@ $(document).ready(function () {
                //     var name = utellyResp.results[i].name; //Loop through UTELLY Json to get movie name                   
                //     var p = $("<p>").text(name); //Setup a <p> tage for name
               // 
-               //    p.addClass("title")
+               
                //     movieDiv.attr("data-name", name);
                 //    var movieImage = $("<img>"); //creates an <img> tag on HTML
                 //    movieImage.attr("src", utellyResp.results[i].picture); //Set img src attribute
@@ -109,7 +91,7 @@ $(document).ready(function () {
                //     console.log(provider);
 
              //   }
-
+         
              var movies = utellyResp.results
              movies.map(movie => {
                  var movieHtml = 
@@ -117,7 +99,7 @@ $(document).ready(function () {
                                  <div class='movieDiv float-left'>
                                          <img src='${movie.picture}' />
                                          <p data-name='${movie.name}'>${movie.name}</p>
-                                       
+                                        
                                  </div>
                          </a>`;
                  $("#movie-view").append(movieHtml);
@@ -128,52 +110,6 @@ $(document).ready(function () {
       
         localStorage.setItem("storageName", movieName);
 
-
-
-
-
-
-=======
-                //    console.log(utellyResp);
-                //   console.log(utellyResp.results);
-                //    console.log(utellyResp.results[0].name);
-                //    console.log(utellyResp.results[0].locations[0].display_name);
-                //    console.log(utellyResp.results[0].locations[0].url);
-                //Loop through to get movie name
-                for (i = 0; i < utellyResp.results.length; i++) {
-                    
-                    var movieDiv = $("<div>"); //Jquery to make a Movie Div
-                    movieDiv.addClass("movieDiv float-left"); //Adding Bootstrap Class to position images
-                    var name = utellyResp.results[i].name; //Loop through UTELLY Json to get movie name 
-                    var p = $("<p>").text(name); //Setup a <p> tage for name
-                    p.attr("data-name", name);//gives each <p> tag matching data name with name
-                    var movieImage = $("<img>"); //creates an <img> tag on HTML
-                    movieImage.attr("src", utellyResp.results[i].picture); //Set img src attribute
-                    link = $("<a id=movLink>");
-                    link.attr("href", "movie.html");
-                    link.addClass("link");
-                    link.addClass("rounded"); //Adds Bootstrap class to round edges of image   
-                    link.attr("target", "_blank")
-                    movieDiv.prepend(p); //Adds <p> before the movie image to the div   
-                    movieDiv.prepend(movieImage); //Adds the movieimage to the div   
-                    link.append(movieDiv);
-                    $("#movie-view").append(link); // Appends the DIv to the movie-view section of HTML   
-                    
-                }
-
-                $(document).on("click", "#movLink", function(){
-    
-                    movLink = localStorage.setItem('${this.data()}')
-                    console.log(movLink)
-                
-                })
-                //Loop to get streaming service
-                for (a = 0; a < utellyResp.results[0].locations.length; a++) {
-                    
-                    provider = (utellyResp.results[0].locations[a].display_name);
-                    console.log(provider);
-                }
->>>>>>> 9885e4fb350630b8a79defc1c8e87c6ae218e8a8
             });
     });
 
