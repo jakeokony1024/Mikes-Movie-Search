098c3554d46b3286e587511b14ec4ba20d56abc0
Jakeokony1024 <jakeokony@gmail.com>
2021-07-23 22:51:31 -0400

    added flexbox and styling to search results

--- assets/javascript/config.js
+++ assets/javascript/config.js
@@ -48,63 +48,18 @@ $(document).ready(function() {
         console.log(utellyResp);
         console.log(utellyResp.results);
         console.log(utellyResp.results[0].name);
-        console.log(utellyResp.results[0].locations[0].display_name);
-        console.log(utellyResp.results[0].locations[0].url);
-
-        //Loop through to get movie name
-        ///   for (i = 0; i < utellyResp.results.length; i++) {
-        //      //console.log(utellyResp.results[i].name);
-        //     var movieDiv = $("<div>"); //Jquery to make a Movie Div
-        //   movieDiv.addClass("movieDiv float-left"); //Adding Bootstrap Class to position images
-
-        //     var name = utellyResp.results[i].name; //Loop through UTELLY Json to get movie name
-        //     var p = $("<p>").text(name); //Setup a <p> tage for name
-        //
-
-        //     movieDiv.attr("data-name", name);
-        //    var movieImage = $("<img>"); //creates an <img> tag on HTML
-        //    movieImage.attr("src", utellyResp.results[i].picture); //Set img src attribute
-        // movieImage.attr("dataname", name);
-        //   movieImage.addClass("imgSrc")
-        //  link = $("<a>");
-
-        //    link.data("href", "movie.html");
-        //    link.data("movieName", name);
-
-        //   link.attr("href", "movie.html");
-        //   link.addClass("movieAtag")
-        //   link.addClass("link");
-        //   link.addClass("rounded");
-        //   link.attr("target", "_blank")
-        //   movieDiv.prepend(p); //Adds <p> before the movie image to the div
-        //  movieDiv.prepend(movieImage); //Adds the movieimage to the div
-        //  link.append(movieDiv);
-        //    $("#movie-view").append(link); // Appends the DIv to the movie-view section of HTML
-
-        //Get name and send to local storage
-
-        //Original Code for looping and finding the search results
-        //$("#movie-view").append("<ul>"+utellyResp.results[i].name+"</ul>");
-        //$("#movie-view").append("<a href ='https://mnezz1131.github.io/mikes-movie/movie.html'> <img src= "+utellyResp.results[i].picture+"></a>");
-        // }
-
-        //Loop to get streaming service
-        // for (a = 0; a < utellyResp.results[0].locations.length; a++) {
-        // console.log(utellyResp.results[0].locations[a].display_name);
-        //   provider = (utellyResp.results[0].locations[a].display_name);
-        //     console.log(provider);
-
-        //   }
-
+   
         var movies = utellyResp.results;
         movies.map(movie => {
-          var movieHtml = `<a href='movie.html' data-moviename='${movie.name}' class='link movieAtag rounded' target='_blank'>
-                                 <div class='movieDiv float-left'>
-                                         <img src='${movie.picture}' />
-                                         <p data-name='${movie.name}' class ='movPTitle'>${movie.name}</p>
-                                        
-                                 </div>
-                         </a>`;
+          var movieHtml = 
+          `<div class='search-results'>
+                <div class='movieDiv'>
+                  <a href='movie.html' data-moviename='${movie.name}' class='link movieAtag rounded'>
+                    <img class='results-image' src='${movie.picture}' />
+                    <p data-name='${movie.name}' class ='movPTitle'>${movie.name}</p>
+                  </a>
+                </div>
+          </div>`;
           $("#movie-view").prepend(movieHtml);
         });
         $(document).on("click", ".movieAtag", function() {
